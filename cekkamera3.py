49ed34871110c4e91902307cf89d2c7598e8970c
RamaSeptian <Ramaseptian045@gmail.com>
2025-09-22 13:23:30 +0700

    Update cekkamera3.py

--- cekkamera3.py
+++ cekkamera3.py
@@ -1,14 +1,15 @@
 import cv2
 
-rtsp_url = "rtsp://admin:BABKQU@192.168.196.110:554/h264/ch1/main/av_stream"
-cap = cv2.VideoCapture(rtsp_url)
+url = "rtsp://admin:BABKQU@192.168.196.110:554/h264/ch1/main/av_stream"
+cap = cv2.VideoCapture(url, cv2.CAP_FFMPEG)  # pakai backend ffmpeg
 
 if not cap.isOpened():
-    print("❌ Gagal membuka RTSP stream")
+    print("❌ Tidak bisa membuka stream")
 else:
-    ret, frame = cap.read()
-    if ret:
-        print("✅ RTSP stream berhasil dibuka, ukuran frame:", frame.shape)
-    else:
-        print("⚠️ RTSP terbuka tapi tidak ada frame")
+    for i in range(50):  # baca 50 frame pertama
+        ret, frame = cap.read()
+        if not ret:
+            print("⚠️ Gagal membaca frame ke-", i)
+            break
+        print(f"✅ Frame {i} terbaca: {frame.shape}")
 cap.release()
